9e923e84d1cbfe7beca97bad816d50bc97aa1059
Jorge Rufat <deartist22@gmail.com>
2019-06-18 23:50:59 -0500

    Event listener added to verify the form

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
 //Variables
-
+const form = document.getElementById('request-quote');
 
 //EventListeners
-document.addEventListener('DOMContentLoaded', function() {
-    //Create the <options> for the years
-    const html = new HTMLUI();
-    html.displayYears();
-    console.log( html.displayYears());
-})
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', function() {
+        //Create the <options> for the years
+        const html = new HTMLUI();
+        html.displayYears();
+        console.log( html.displayYears());
+    })
 
+    //When the form is submitted
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+
+        //Read the values from the form
+        const make = document.getElementById('make').value;
+        const year = document.getElementById('year').value;
+
+        //Read the radio buttons
+        const level = document.querySelector('input[name="level"]:checked').value;
+
+        //Check that all fields have something
+        if(make === '' || year === ''|| level === ''){
+            console.log('Error');
+        } else {
+            console.log('Working');
+        }
+    })
+}
 
 //Objects
 
